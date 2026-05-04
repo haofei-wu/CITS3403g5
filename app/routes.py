e3f8fadd4256ec412565eb0b6d1d899591809ec5
Himakshi28 <24284436@student.uwa.edu.au>
2026-05-05 00:20:01 +0800

    Fix 403 issue by redirecting index to login

--- app/routes.py
+++ app/routes.py
@@ -8,8 +8,7 @@ from werkzeug.security import generate_password_hash, check_password_hash
 # ------------------ HOME ------------------
 @app.route('/')
 def index():
-    return render_template('index.html')
-
+    return redirect(url_for('login'))
 
 # ------------------ LOGIN ------------------
 @app.route('/login', methods=['GET', 'POST'])
